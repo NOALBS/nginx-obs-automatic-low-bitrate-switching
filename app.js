bea7a2543c4ffa763db987680622b13eeb7d9e70
Brian Spit <brian@mythenatic.com>
2018-12-28 01:37:35 +0100

    Automatically detect online/offline, only switch when on scenes set in config and added more variables to config

--- app.js
+++ app.js
@@ -10,38 +10,66 @@ const app = express();
 const parseString = xml2js.parseString;
 let isLive = false;
 
-obs.connect({ address: config.ipObs, password: config.password })
-    .then(function () { console.log(`Success! We're connected & authenticated.`) })
-    .catch(function (err) { console.error("Can't connect to OBS, did you enter the correct ip?") });
+obs
+  .connect({ address: config.ipObs, password: config.passwordObs })
+  .then(() => console.log(`Success! We're connected & authenticated.`))
+  .catch(() =>
+    console.error("Can't connect to OBS, did you enter the correct ip?")
+  );
 
-app.get('/online', function (req, res) {
-    isLive = true;
-
-    obs.setCurrentScene({ 'scene-name': config.normalScene });
-    res.sendStatus(200);
+app.get("/online", (req, res) => {
+  isLive = true;
+  res.sendStatus(200);
 });
 
-app.get('/offline', function (req, res) {
-    isLive = false;
-
-    obs.setCurrentScene({ 'scene-name': config.offlineScene });
-    res.sendStatus(200);
+app.get("/offline", (req, res) => {
+  isLive = false;
+  res.sendStatus(200);
 });
 
-setInterval(function () {
-    if (isLive) {
-        request('http://' + config.ipNginx + '/stat', function (error, response, body) {
-            if (error) throw error;
+setInterval(() => {
+  request(`http://${config.ipNginx}/stat`, (error, response, body) => {
+    if (error) console.log("Can't access nginx");
 
-            parseString(body, async function (err, result) {
-                let bitrate = result.rtmp.server[0].application[0].live[0].stream[0].bw_video[0] / 1024;
-                const currentScene = await obs.GetCurrentScene();
+    parseString(body, async (err, result) => {
+      const currentScene = await obs.GetCurrentScene();
+      const canSwitch =
+        currentScene.name == config.lowBitrateScene ||
+        currentScene.name == config.normalScene ||
+        currentScene.name == config.offlineScene;
 
-                bitrate <= config.lowBitrateTrigger && currentScene.name !== config.lowBitrateScene && bitrate !== 0 && (obs.setCurrentScene({ 'scene-name': config.lowBitrateScene }), console.log(`Low bitrate detected switching to scene ${config.lowBitrateScene}.`));
-                bitrate > config.lowBitrateTrigger && currentScene.name !== config.normalScene && (obs.setCurrentScene({ 'scene-name': config.normalScene }), console.log(`Switching back to scene ${config.normalScene}.`));
-            });
-        });
-    }
-}, 2000);
+      try {
+        const bitrate =
+          result.rtmp.server[0].application[0].live[0].stream[0].bw_video[0] /
+          1024;
 
-app.listen(3000, function () { console.log('App listening on port 3000!') });
+        isLive = true;
+
+        isLive &&
+          canSwitch &&
+          (bitrate <= config.lowBitrateTrigger &&
+            currentScene.name !== config.lowBitrateScene &&
+            bitrate !== 0 &&
+            (obs.setCurrentScene({ "scene-name": config.lowBitrateScene }),
+            console.log(
+              `Low bitrate detected switching to scene ${
+                config.lowBitrateScene
+              }.`
+            )),
+          bitrate > config.lowBitrateTrigger &&
+            currentScene.name !== config.normalScene &&
+            (obs.setCurrentScene({ "scene-name": config.normalScene }),
+            console.log(`Switching back to scene ${config.normalScene}.`)));
+      } catch (e) {
+        isLive = false;
+        canSwitch &&
+          (obs.setCurrentScene({ "scene-name": config.offlineScene }),
+          console.log("Error receiving current bitrate or steam is offline"));
+      }
+    });
+  });
+}, config.requestMs);
+
+app.listen(config.PORT, () => {
+  console.log(`App listening on port ${config.PORT}!`);
+});
