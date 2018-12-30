3f828a0f600da1ece199912e0637a9141743eeff
Brian Spit <brian@mythenatic.com>
2018-12-31 00:26:55 +0100

    Resolves #1

--- src/components/ChatCommands.js
+++ src/components/ChatCommands.js
@@ -1,10 +1,12 @@
+import config from "../../config";
+
 class ChatCommands {
   constructor(_, prefix) {
     this.ws = _.ws;
     this.channel = _.username;
     this.obs = _.obs;
     this.prefix = prefix;
-    this.options = ["host", "unhost", "start", "stop", "switch"];
+    this.options = ["host", "unhost", "start", "stop", "switch", "raid"];
   }
 
   host(username) {
@@ -13,8 +15,9 @@ class ChatCommands {
 
       setTimeout(() => {
         this.stop();
-      }, 5000);
+      }, config.stopStreamOnHostInterval);
     } else {
+      this.ws.send(`PRIVMSG #${this.channel} :Error no username`);
       console.log("Error executing host command no username");
     }
   }
@@ -23,12 +26,27 @@ class ChatCommands {
     this.ws.send(`PRIVMSG #${this.channel} :/unhost`);
   }
 
+  raid(username) {
+    if (username != null) {
+      this.ws.send(`PRIVMSG #${this.channel} :/raid ${username}`);
+
+      setTimeout(() => {
+        this.stop();
+      }, config.stopStreamOnRaidInterval);
+    } else {
+      console.log("Error executing host command no username");
+      this.ws.send(`PRIVMSG #${this.channel} :Error no username`);
+    }
+  }
+
   async start() {
     // start streaming
     try {
       await this.obs.startStreaming();
+      this.ws.send(`PRIVMSG #${this.channel} :Successfully started stream`);
     } catch (e) {
       console.log(e);
+      this.ws.send(`PRIVMSG #${this.channel} :Error ${e.error}`);
     }
   }
 
@@ -36,8 +54,11 @@ class ChatCommands {
     // stop streaming
     try {
       await this.obs.stopStreaming();
+
+      this.ws.send(`PRIVMSG #${this.channel} :Successfully stopped stream`);
     } catch (e) {
-      console.log(e);
+      console.log(e.error);
+      this.ws.send(`PRIVMSG #${this.channel} :${e.error}`);
     }
   }
 
@@ -45,6 +66,12 @@ class ChatCommands {
     // switch scene
     try {
       await this.obs.setCurrentScene({ "scene-name": sceneName });
+
+      this.ws.send(
+        `PRIVMSG #${
+          this.channel
+        } :Scene successfully switched to "${sceneName}"`
+      );
     } catch (e) {
       console.log(e);
     }
