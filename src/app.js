95b951ea947bf21b0be5a034252b4166e4ec324e
Brian Spit <brian@mythenatic.com>
2020-05-02 02:52:39 +0200

    Remove env variable

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import ObsSwitcher from "./components/ObsSwitcher";
 import Chat from "./components/Chat";
 import NodeMediaServer from "./components/NodeMediaServer";
 import config from "../config";
+import { version } from "../package.json";
 
 console.log(`
     ███╗   ██╗ ██████╗  █████╗ ██╗     ██████╗ ███████╗
@@ -9,7 +10,7 @@ console.log(`
     ██╔██╗ ██║██║   ██║███████║██║     ██████╔╝███████╗
     ██║╚██╗██║██║   ██║██╔══██║██║     ██╔══██╗╚════██║
     ██║ ╚████║╚██████╔╝██║  ██║███████╗██████╔╝███████║
-    ╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═════╝ ╚══════╝ v${process.env.npm_package_version}
+    ╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═════╝ ╚══════╝ v${version}
 `);
 
 const obs = new ObsSwitcher(
@@ -22,7 +23,12 @@ const obs = new ObsSwitcher(
 );
 
 if (config.twitchChat.enable) {
-    const chat = new Chat(config.twitchChat.botUsername, config.twitchChat.oauth, config.twitchChat.channel, obs);
+    const chat = new Chat(
+        config.twitchChat.botUsername,
+        config.twitchChat.oauth,
+        config.twitchChat.channel,
+        obs
+    );
 }
 
 const nodeMediaServer = new NodeMediaServer(config.nodeMediaServer, obs);
