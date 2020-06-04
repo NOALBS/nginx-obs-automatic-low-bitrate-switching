584752865c976fc723343ebf88af2406538c5dd4
Seb Patane <sebbity@gmail.com>
2020-06-05 03:26:40 +1000

    Nodejs 8 doesn't let us directly set the whole console variable - It fails with 'TypeError: Cannot set property console of #<Object> which has only a getter' - Instead, just catch the error and set console.log (which it is happy with), since that's all node-media-server seems to use - Quietly nudge the user to upgrade from nodejs as well, since it is pretty old now - Fixes https://github.com/715209/nginx-obs-automatic-low-bitrate-switching/issues/30

--- src/components/NodeMediaServer.js
+++ src/components/NodeMediaServer.js
@@ -8,7 +8,12 @@ class NodeMediaServerManager {
             return;
         }
 
-        console = signale.scope("NMS");
+        try {
+            console = signale.scope("NMS");
+        } catch(e) {
+            console.log = signale.scope("NMS").log;
+            signale.scope("NMS").warn(`Couldn't set console for node-media-server. Consider upgrading to node 10 or higher to resovle this problem (error: ${e})`);
+        }
 
         this.obsSwitcher = obsSwitcher;
 
