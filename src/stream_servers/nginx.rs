54c752f99107d950d881d60fc71a32d9b2dfb8ee
Brian Spit <5977687+715209@users.noreply.github.com>
2021-11-14 15:59:14 +0100

    Fix nginx parsing frame_rate and audio codec

--- src/stream_servers/nginx.rs
+++ src/stream_servers/nginx.rs
@@ -43,7 +43,7 @@ pub struct Meta {
 pub struct Video {
     width: u32,
     height: u32,
-    frame_rate: u32,
+    frame_rate: f64,
     codec: String,
     profile: Option<String>,
     compat: Option<u32>,
@@ -52,7 +52,7 @@ pub struct Video {
 
 #[derive(Deserialize, Debug)]
 pub struct Audio {
-    codec: String,
+    codec: Option<String>,
     profile: Option<String>,
     channels: Option<u32>,
     sample_rate: Option<u32>,
@@ -186,7 +186,7 @@ impl StreamServersCommands for Nginx {
 
         let a_info = format!(
             "{} {} {}Hz, {} channels",
-            audio.codec,
+            audio.codec.unwrap_or_default(),
             audio.profile.unwrap_or_default(),
             audio.sample_rate.unwrap_or_default(),
             audio.channels.unwrap_or_default(),
@@ -210,3 +210,81 @@ impl Bsl for Nginx {}
 //         }
 //     }
 // }
+//
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn invalid_digit() {
+        let text = r#"
+            <?xml version="1.0" encoding="utf-8" ?>
+            <?xml-stylesheet type="text/xsl" href="/stat.xsl" ?>
+            <rtmp>
+                <nginx_version>1.17.6</nginx_version>
+                <nginx_rtmp_version>1.1.7.11-dev</nginx_rtmp_version>
+                <compiler>gcc 9.3.0 (Ubuntu 9.3.0-17ubuntu1~20.04) </compiler>
+                <built>Feb 15 2021 01:06:01</built>
+                <pid>944</pid>
+                <uptime>1781693</uptime>
+                <naccepted>262</naccepted>
+                <bw_in>0</bw_in>
+                <bytes_in>1185796040</bytes_in>
+                <bw_out>0</bw_out>
+                <bytes_out>646442932</bytes_out>
+                <server>
+                    <application>
+                        <name>publish</name>
+                        <live>
+                            <stream>
+                                <name>test</name>
+                                <time>1832</time>
+                                <bw_in>0</bw_in>
+                                <bytes_in>766897</bytes_in>
+                                <bw_out>0</bw_out>
+                                <bytes_out>0</bytes_out>
+                                <bw_audio>0</bw_audio>
+                                <bw_video>0</bw_video>
+                                <bw_data>0</bw_data>
+                                <client>
+                                    <id>3192</id>
+                                    <address>123.123.123.123</address>
+                                    <port>59796</port>
+                                    <time>2008</time>
+                                    <flashver>FMLE/3.0 (compatible; Larix/nul</flashver>
+                                    <bytes_in>768181</bytes_in>
+                                    <bytes_out>409</bytes_out>
+                                    <dropped>0</dropped>
+                                    <avsync></avsync>
+                                    <timestamp>3361</timestamp>
+                                    <publishing/>
+                                    <active/>
+                                </client>
+                                <meta>
+                                    <video>
+                                        <width>1280</width>
+                                        <height>720</height>
+                                        <frame_rate>0.000</frame_rate>
+                                        <codec>H264</codec>
+                                        <profile>Baseline</profile>
+                                        <compat>192</compat>
+                                        <level>3.1</level>
+                                    </video>
+                                    <audio></audio>
+                                </meta>
+                                <nclients>1</nclients>
+                                <publishing/>
+                                <active/>
+                            </stream>
+                            <nclients>1</nclients>
+                        </live>
+                    </application>
+                </server>
+            </rtmp>
+        "#;
+
+        let parsed: NginxRtmpStats = quick_xml::de::from_str(&text).unwrap();
+        println!("{:#?}", parsed);
+    }
+}
