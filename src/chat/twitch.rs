542dd2438527de0b60662c59c9e0291c388aeaaf
Brian Spit <brian@715209.net>
2025-11-07 04:53:48 +0100

    Ignore messages from shared chat
    Closes #193

--- src/chat/twitch.rs
+++ src/chat/twitch.rs
@@ -75,6 +75,14 @@ impl Twitch {
                     }
                 }
                 message::ServerMessage::Privmsg(msg) => {
+                    // Ignore messages not originating from the current channel
+                    let source_room_id = msg.source.tags.0.get("source-room-id");
+                    if let Some(Some(source_room_id)) = source_room_id
+                        && &msg.channel_id != source_room_id
+                    {
+                        continue;
+                    }
+
                     let permission =
                         msg.badges
                             .iter()
